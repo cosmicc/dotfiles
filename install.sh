a337bd6cf8616ed2bd1cc73e2a7fcb8c2eae1395
cosmicc <ianperry99@gmail.com>
2020-10-24 15:47:45 -0400

    update

--- install.sh
+++ install.sh
@@ -1,1 +1,34 @@
 git clone https://github.com/cosmicc/dotfiles.git
+print_debug() {
+	  echo "$1"
+  }
+
+print_info() {
+	  echo -e "\033[33m$1\033[0m"
+  }
+
+print_noop() {
+	  echo -e "\033[35m$1\033[0m"
+  }
+
+print_success() {
+	  echo -e "\033[32m$1\033[0m"
+  }
+
+print_error() {
+	  echo -e "\033[31m$1\033[0m"
+  }
+
+die() {
+	  echo $1
+	    exit ${2:-1}
+    }
+
+echo "Installing zsh shell..."
+apt-get -qq install zsh -y
+echo "Installing oh-my-zsh"
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp dircolors /etc/zsh/dircolors
+cp promptline.sh /etc/zsh/promptline.sh
+echo test -r /etc/zsh/dircolors && eval "$(dircolors -b /etc/zsh/dircolors)" || eval "$(dircolors -b)" >> /etc/zsh/zshrc
+echo source /etc/zsh/promptline.sh >> /etc/zsh/zshrc
