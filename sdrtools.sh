bcf3737ac04055bc6df44211bf1aa856469a240d
Ian Perry <Ianperry99@gmail.com>
2020-10-25 23:02:19 -0400

    Update sdrtools.sh

--- sdrtools.sh
+++ sdrtools.sh
@@ -1,1 +1,10 @@
+YEL='\033[1;33m'
+CYN='\033[1;36m' 
+NC='\033[0m'
+
+
+
+echo "${CYN}Installing Prerequisites...${NC}"
+sudo apt -qq install libwxbase3.0-0v5 libwxgtk3.0-gtk3-0v5 python3-future python3-sip python3-wxgtk4.0 -y
+
 
