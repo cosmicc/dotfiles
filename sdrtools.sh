2327f114362fce57a8bdf15df53df2bc9c2f43b1
Ian Perry <Ianperry99@gmail.com>
2020-10-26 21:44:20 -0400

    Update sdrtools.sh

--- sdrtools.sh
+++ sdrtools.sh
@@ -3,7 +3,7 @@ CYN='\033[1;36m'
 NC='\033[0m'
 
 echo "${CYN}Installing Prerequisites...${NC}"
-sudo apt -qq install libwxbase3.0-0v5 libwxgtk3.0-gtk3-0v5 python3-future python3-sip python3-wxgtk4.0 libfltk1.3-dev libpng-dev samplerate-programs libc6 libfltk-images1.3 libfltk1.3 libfltk1.3-dev libgcc1 libhamlib2 libhamlib-dev libpng-dev portaudio19-dev libportaudio2 libportaudiocpp0 libflxmlrpc1v5 libpulse0 libpulse-dev librpc-xml-perl libsamplerate0 libsamplerate0-dev libsndfile1 libsndfile1-dev libstdc++6 libx11-6 libterm-readline-gnu-perl gfortran libfftw3-dev qt5-qmake qtbase5-dev libqt5multimedia5 qtmultimedia5-dev libqt5serialport5 libqt5serialport5-dev qt5-default qtscript5-dev libssl-dev qttools5-dev qttools5-dev-tools libqt5svg5-dev libqt5webkit5-dev libsdl2-dev libasound2 libxmu-dev libxi-dev freeglut3-dev libasound2-dev libjack-jackd2-dev libxrandr-dev libqt5xmlpatterns5-dev libqt5xmlpatterns5 libvolk2-bin gnuradio gnuradio-dev gr-osmosdr libosmosdr-dev libqt5svg5-dev librtlsdr-dev osmo-sdr portaudio19-dev qt5-default gr-osmosdr gr-gsm -y
+sudo apt -qq install libwxbase3.0-0v5 libwxgtk3.0-gtk3-0v5 python3-future python3-sip python3-wxgtk4.0 libfltk1.3-dev libpng-dev samplerate-programs libc6 libfltk-images1.3 libfltk1.3 libfltk1.3-dev libgcc1 libhamlib2 libhamlib-dev libpng-dev portaudio19-dev libportaudio2 libportaudiocpp0 libflxmlrpc1v5 libpulse0 libpulse-dev librpc-xml-perl libsamplerate0 libsamplerate0-dev libsndfile1 libsndfile1-dev libstdc++6 libx11-6 libterm-readline-gnu-perl gfortran libfftw3-dev qt5-qmake qtbase5-dev libqt5multimedia5 qtmultimedia5-dev libqt5serialport5 libqt5serialport5-dev qt5-default qtscript5-dev libssl-dev qttools5-dev qttools5-dev-tools libqt5svg5-dev libqt5webkit5-dev libsdl2-dev libasound2 libxmu-dev libxi-dev freeglut3-dev libasound2-dev libjack-jackd2-dev libxrandr-dev libqt5xmlpatterns5-dev libqt5xmlpatterns5 libvolk2-bin gnuradio gnuradio-dev gr-osmosdr libosmosdr-dev libqt5svg5-dev librtlsdr-dev osmo-sdr portaudio19-dev qt5-default gr-osmosdr gr-gsm pkg-config libglib2.0-dev libgtk-3-dev libgoocanvas-2.0-dev libtool intltool autoconf automake libcurl4-openssl-dev -y
 
 echo "${CYN}Installing Chirp...${NC}"
 sudo apt -qq install chirp -y
@@ -158,6 +158,7 @@ if [ $answer = "y" ]; then
     mkdir build
     cd build
     cmake -Dhamlib_LIBRARY_DIRS=/usr/lib/aarch64-linux-gnu -DJS8_USE_HAMLIB_THREE=1 ../
+    sed -i '1s/^/#define JS8_USE_HAMLIB_THREE\n /' /opt/build/radio-tools/js8call/HamlibTransceiver.hpp
     make
 fi
 
@@ -173,15 +174,61 @@ if [ $answer = "y" ]; then
     else
         echo "${CYN}Updaing GQRX Source...${NC}"
         cd gqrx
+        rm -r build
         git pull
     fi
     mkdir build
     cd build
     cmake ../
+    make
+    sudo make install
 fi
 
+echo -n "${YEL}Download, Compile & Install Dump1090 (y/n)? ${NC}" 
+read answer
+if [ $answer = "y" ]; then 
+    echo "${CYN}Installing Dump1090...${NC}"
+    cd /opt/build/radio-tools
+    if [ ! -d "/opt/build/radio-tools/dump1090" ]; then
+        echo "${CYN}Cloning Dump1090 Source...${NC}"
+        git clone https://github.com/antirez/dump1090.git
+        cd dump1090
+    else
+        echo "${CYN}Updaing Dump1090 Source...${NC}"
+        cd dump1090
+        sudo rm dump1090 -f
+        git pull
+    fi
+    make
+    sudo cp dump1090 /usr/local/bin -f
+    sudo chmod ugo+x /usr/local/bin/dump1090
+fi
 
+echo -n "${YEL}Download, Compile & Install VirtualFlight Radar (y/n)? ${NC}" 
+read answer
+if [ $answer = "y" ]; then 
+    echo "${CYN}Installing VirtualFlight Prerequisites...${NC}"
+    sudo apt -qq install mono-complete -y
+    echo "${CYN}Installing VirtualFlight Radar...${NC}"
+    cd /opt/build/radio-tools
+    wget http://www.virtualradarserver.co.uk/Files/VirtualRadar.tar.gz
+    rm -r virtualradar
+    mkdir virtualradar
+    tar xvfz VirtualRadar.tar.gz -C virtualradar
+    rm VirtualRadar.tar.gz
+    cd virtualradar
+    
+fi
 
+echo -n "${YEL}Download, Compile & Install GPredict (y/n)? ${NC}" 
+read answer
+if [ $answer = "y" ]; then 
+    echo "${CYN}Installing GPredict...${NC}"
+    cd /opt/build/radio-tools
+    git clone https://github.com/csete/gpredict.git
+    cd gpredict
+    
+fi
 
 
 
